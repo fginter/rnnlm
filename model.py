c89690c4c625e09e1be3806c586ac2dedb86dea5
Filip Ginter <ginter@cs.utu.fi>
2019-03-25 15:20:21 +0200

    badassmodel

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Input, Embedding, Dense, TimeDistributed, Dropout, BatchNormalization, Add
+from tensorflow.keras.layers import Input, Embedding, Dense, TimeDistributed, Dropout, BatchNormalization, Add, SpatialDropout1D, Lambda
 from tensorflow.keras.layers import CuDNNLSTM as LSTM
 
 
@@ -70,4 +70,55 @@ class LSTMModel_2:
         dec=Dense(vocab_size,activation="softmax",name="decision")
         dec_td=TimeDistributed(dec)(proj_weights)
         mod=tf.keras.Model(inputs=[inp],outputs=[dec_td])
+        return mod
+
+
+class LSTMModel_3:
+
+    def __init__(self,vocab_size):
+        self.model=self.make_model(vocab_size)
+
+    def make_model(self,vocab_size):
+        inp=Input(shape=(None,),dtype=tf.int64,name="input_sent")
+        emb_layer=Embedding(input_dim=vocab_size,output_dim=768,embeddings_initializer=tf.keras.initializers.Constant(0.01))
+        emb=emb_layer(inp)
+        emb_do=SpatialDropout1D(0.3)(emb)
+        emb_drop_n=BatchNormalization()(emb_do)
+        rnn1=LSTM(768,return_sequences=True)(emb_drop_n)
+        rnn1_n=BatchNormalization()(rnn1)
+        rnn2=LSTM(768,return_sequences=True)(rnn1_n)
+        rnn2_n=BatchNormalization()(rnn2)
+        rnn3=LSTM(768,return_sequences=True)(rnn2_n)
+        rnn3_n=BatchNormalization()(rnn3)
+        proj_weights=TimeDistributed(Dense(512,activation="tanh"))(Add()([rnn1_n,rnn2_n,rnn3_n]))
+        dec=Dense(vocab_size,activation="softmax",name="decision")
+        dec_td=TimeDistributed(dec)(proj_weights)
+        mod=tf.keras.Model(inputs=[inp],outputs=[dec_td])
+
+        return mod
+
+
+class LSTMModel_4:
+
+    def __init__(self,vocab_size):
+        self.model=self.make_model(vocab_size)
+
+    def make_model(self,vocab_size):
+        inp=Input(shape=(None,),dtype=tf.int64,name="input_sent")
+        emb_layer=Embedding(input_dim=vocab_size,output_dim=1500,embeddings_initializer=tf.keras.initializers.Constant(0.01))
+        emb=emb_layer(inp)
+        emb_do=SpatialDropout1D(0.5)(emb)
+        emb_drop_n=BatchNormalization()(emb_do)
+        rnn1=LSTM(1500,return_sequences=True)(emb_drop_n)
+        rnn1_n=BatchNormalization()(rnn1)
+        rnn2=LSTM(1500,return_sequences=True)(rnn1_n)
+        rnn2_n=BatchNormalization()(rnn2)
+        rnn3=LSTM(1500,return_sequences=True)(rnn2_n)
+        rnn3_n=BatchNormalization()(rnn3)
+        proj_weights=TimeDistributed(Dense(1500))(Add()([rnn1_n,rnn2_n,rnn3_n]))
+        proj_weights_gelu=Lambda(lambda x: tf.multiply(x,tf.nn.sigmoid(tf.scalar_mul(1.702,x))))(proj_weights)
+        dec=Dense(vocab_size,activation="softmax",name="decision")
+        dec_td=TimeDistributed(dec)(proj_weights_gelu)
+        mod=tf.keras.Model(inputs=[inp],outputs=[dec_td])
+
         return mod
